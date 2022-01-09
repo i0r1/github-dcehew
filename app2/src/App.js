db77f38a4cd2911a8e8317c6ad73b37859556327
i0r1 <iori7891@gmail.com>
2022-01-09 15:00:17 -0700

    1 file modified

--- app2/src/App.js
+++ app2/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { useFeature } from '@optimizely/react-sdk';
 
 const App = () => {
-  const [isFeatureEnabled, variables] = useFeature('analytics_page', {});
+  // const [isFeatureEnabled, variables] = useFeature('analytics_page', {});
   console.log(isFeatureEnabled);
   return (
     <div
